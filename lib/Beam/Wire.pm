959bb361e570b35368f8da6007ea4b92c3609dd9
Doug Bell <preaction@cpan.org>
2014-11-02 23:07:44 +0000

    Import of PREACTION/Beam-Wire-1.007 from CPAN.
    gitpan-cpan-distribution: Beam-Wire
    gitpan-cpan-version:      1.007
    gitpan-cpan-path:         PREACTION/Beam-Wire-1.007.tar.gz
    gitpan-cpan-author:       PREACTION
    gitpan-cpan-maturity:     released

--- lib/Beam/Wire.pm
+++ lib/Beam/Wire.pm
@@ -1,6 +1,6 @@
 package Beam::Wire;
 # ABSTRACT: A Dependency Injection Container
-$Beam::Wire::VERSION = '1.006';
+$Beam::Wire::VERSION = '1.007';
 use strict;
 use warnings;
 
@@ -350,13 +350,13 @@ sub BUILD {
 
 
 package Beam::Wire::Exception;
-$Beam::Wire::Exception::VERSION = '1.006';
+$Beam::Wire::Exception::VERSION = '1.007';
 use Moo;
 with 'Throwable';
 
 
 package Beam::Wire::Exception::Service;
-$Beam::Wire::Exception::Service::VERSION = '1.006';
+$Beam::Wire::Exception::Service::VERSION = '1.007';
 use Moo;
 use MooX::Types::MooseLike::Base qw( :all );
 extends 'Beam::Wire::Exception';
@@ -374,13 +374,13 @@ has file => (
 
 
 package Beam::Wire::Exception::NotFound;
-$Beam::Wire::Exception::NotFound::VERSION = '1.006';
+$Beam::Wire::Exception::NotFound::VERSION = '1.007';
 use Moo;
 extends 'Beam::Wire::Exception::Service';
 
 
 package Beam::Wire::Exception::InvalidConfig;
-$Beam::Wire::Exception::InvalidConfig::VERSION = '1.006';
+$Beam::Wire::Exception::InvalidConfig::VERSION = '1.007';
 use Moo;
 extends 'Beam::Wire::Exception::Service';
 
@@ -396,7 +396,7 @@ Beam::Wire - A Dependency Injection Container
 
 =head1 VERSION
 
-version 1.006
+version 1.007
 
 =head1 SYNOPSIS
 
